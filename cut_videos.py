995a5109f1a376251113adf15635a210df87d1ad
Bendix <bendix@benoto.net>
2026-04-01 20:17:59 +0200

    Base for creating Videos for Powerpoint Presentations

---
+++ cut_videos.py
@@ -0,0 +1,41 @@
+import json
+import subprocess
+
+VIDEO_FILE = "./media/videos/scenes/480p15/TestingScene.mp4"
+SEGMENTS_FILE = "./media/videos/scenes/480p15/segments.json"
+dir = "./splitted"
+
+with open(SEGMENTS_FILE) as f:
+    segments = json.load(f)
+
+for i in range(len(segments)):
+    start = segments[i]["time"]
+    name = segments[i]["name"]
+
+    if i < len(segments) - 1:
+        end = segments[i + 1]["time"]
+    else:
+        end = None  # last segment goes to end
+
+    path = f"{dir}/{i:02d}_{name}.mp4"
+
+    cmd = [
+        "ffmpeg",
+        "-y",
+        "-ss", str(start),
+    ]
+
+    if end is not None:
+        duration = end - start
+        cmd += ["-t", str(duration)]
+
+    cmd += [
+        "-i", VIDEO_FILE,
+        "-c", "copy",
+        path
+    ]
+
+    print("Running:", " ".join(cmd))
+    subprocess.run(cmd)
+
+print("\nDone splitting!")
---
+++ scenes.py
@@ -0,0 +1,42 @@
+from manim import *
+import os, json
+
+class OwnScene(Scene):
+    def __init__(self):
+        super().__init__()
+        self.segments = []
+
+    def segment(self, name):
+        self.wait(0.01)
+        t = self.renderer.time
+        t = round(t, 3)
+        print(f"[SEGMENT] {name} at {t:.2f}s")
+        self.segments.append({"name": name, "time": t})
+
+    def save_segments(self):
+        import os
+        import json
+
+        # This ALWAYS works across versions
+        video_path = self.renderer.file_writer.movie_file_path
+        output_dir = os.path.dirname(video_path)
+
+        path = os.path.join(output_dir, "segments.json")
+
+        with open(path, "w") as f:
+            json.dump(self.segments, f, indent=2)
+
+        print(f"\nSaved segments to: {path}\n")
+
+class TestingScene(OwnScene):
+    def construct(self):
+        self.segment("First_Point")
+        circle = Circle(2, BLUE)
+        self.play(Create(circle), run_time = 2)
+        self.wait(1)
+
+        self.segment("Second_Point")
+        self.play(FadeOut(circle))
+        self.wait(2)
+
+        self.save_segments()
